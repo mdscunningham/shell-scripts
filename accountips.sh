3080376b336452d09198570bea52437f9e76f3f2
Mark Cunningham <mcunningham@nexcess.net>
2014-09-11 21:48:15 -0400

    Add temp fix lookup to domaincheck.sh and accountips.sh
    Add a new column to lookup if the vhost has a temp fix in place
    Will print FIX in the TMP column if there is more than one IP setup.

--- accountips.sh
+++ accountips.sh
@@ -3,21 +3,23 @@ accountips ()
     echo;
     DIR=$PWD;
     cd /home/$(getusr);
-    FORMAT=" %-15s  %-15s  %-3s  %-3s  %s\n";
-    HIGHLIGHT="${BRIGHT}${RED} %-15s  %-15s  %-3s  %-3s  %s${NORMAL}\n";
-    printf "$FORMAT" " ServerIP" " LiveIP" "SSL" "FPM" " DomainName";
-    printf "$FORMAT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash 39)";
+    FORMAT=" %-15s  %-15s  %3s  %3s  %3s  %s\n";
+    HIGHLIGHT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %3s  %s${NORMAL}\n";
+    printf "$FORMAT" " ServerIP" " LiveIP" "SSL" "FPM" "TMP" " DomainName";
+    printf "$FORMAT" "$(dash 15)" "$(dash 15)" "---" "---" "---" "$(dash 39)";
     for x in */html;
     do
         D=$(echo $x | cut -d/ -f1);
+	vhost="/etc/httpd/conf.d/vhost_$D.conf"
         L=$(dig +tries=1 +time=3 +short $1$D | grep -v \; | head -n1);
-        I=$(grep -i virtualhost /etc/httpd/conf.d/vhost_$D.conf 2> /dev/null | head -n1 | awk '{print $2}' | cut -d: -f1);
-        S=$(if grep -q ':443' /etc/httpd/conf.d/vhost_$D.conf &> /dev/null; then echo SSL; fi)
-	F=$(if grep -q 'MAGE_RUN' /etc/httpd/conf.d/vhost_$D.conf &> /dev/null; then echo FIX; fi)
+        I=$(awk '/.irtual.ost/ {print $2}' $vhost 2> /dev/null | head -n1 | cut -d: -f1);
+        S=$(if grep -q ':443' $vhost &> /dev/null; then echo SSL; fi)
+	F=$(if grep -q 'MAGE_RUN' $vhost &> /dev/null; then echo FIX; fi)
+	T=$(if [[ -n $(awk '/.irtual.ost/ {print $3}' $vhost &> /dev/null) ]]; then echo FIX; fi)
         if [[ $I != $L ]]; then
-            printf "$HIGHLIGHT" "$I" "$L" "${S:- - }" "${F:- - }" "$1$D";
+            printf "$HIGHLIGHT" "$I" "$L" "${S:- - }" "${F:- - }" "${T:- - }" "$1$D";
         else
-            printf "$FORMAT" "$I" "$L" "${S:- - }" "${F:- - }" "$1$D";
+            printf "$FORMAT" "$I" "$L" "${S:- - }" "${F:- - }" "${T:- - }" "$1$D";
         fi;
     done;
     echo;
--- domaincheck.sh
+++ domaincheck.sh
@@ -8,44 +8,48 @@
 #
 #!/bin/bash
 
-source colors.sh
+#source colors.sh
 dash(){ for ((i=1; i<=$1; i++)); do printf "-"; done; }
 
-echo;
-FORMAT=" %-15s  %-15s  %-3s  %s\n";
-HIGHLIGHT="${BRIGHT}${RED} %-15s  %-15s  %-3s  %s${NORMAL}\n";
-printf "$FORMAT" " ServerIP" " LiveIP" "SSL" " DomainName";
-printf "$FORMAT" "$(dash 15)" "$(dash 15)" "---" "$(dash 44)";
-for x in /etc/httpd/conf.d/vhost_[^000_]*.conf;
-do
-    D=$(basename $x .conf | cut -d_ -f2);
-    I=$(grep -i virtualhost $x 2> /dev/null | head -n1 | awk '{print $2}' | cut -d: -f1);
-    L=$(dig +time=3 +tries=1 +short $D | grep -v \; | head -n1);
-    if grep ':443' $x &> /dev/null; then
-        S="YES";
-    else
-        S="NO";
-    fi;
-    if [[ $I != $L ]]; then
-        printf "$HIGHLIGHT" "$I" "$L" "$S" "$D";
-    else
-        printf "$FORMAT" "$I" "$L" "$S" "$D";
-    fi;
-done;
-echo
+# echo;
+# FORMAT=" %-15s  %-15s  %-3s  %s\n";
+# HIGHLIGHT="${BRIGHT}${RED} %-15s  %-15s  %-3s  %s${NORMAL}\n";
+# printf "$FORMAT" " ServerIP" " LiveIP" "SSL" " DomainName";
+# printf "$FORMAT" "$(dash 15)" "$(dash 15)" "---" "$(dash 44)";
+# for x in /etc/httpd/conf.d/vhost_[^000_]*.conf;
+# do
+#    D=$(basename $x .conf | cut -d_ -f2);
+#    I=$(grep -i virtualhost $x 2> /dev/null | head -n1 | awk '{print $2}' | cut -d: -f1);
+#    L=$(dig +time=3 +tries=1 +short $D | grep -v \; | head -n1);
+#    if grep ':443' $x &> /dev/null; then
+#        S="YES";
+#    else
+#        S="NO";
+#    fi;
+#    if [[ $I != $L ]]; then
+#        printf "$HIGHLIGHT" "$I" "$L" "$S" "$D";
+#    else
+#        printf "$FORMAT" "$I" "$L" "$S" "$D";
+#    fi;
+# done;
+# echo
 
 #
-# FMT=" ${color}%-15s  %-15s  %-3s  %-3s  %s\n"
-# printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" " Domain"
-# printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash )"
-#
-# echo; for x in /etc/httpd/conf.d/vhost_[^000]*.conf; do
-#   D=$(basename $x .conf | cut -d_ -f2);
-#   V=$(grep -i virtualhost $x | head -1 | awk '{print $2}' | cut -d: -f1);
-#   I=$(dig +short +time=1 +tries=1 $D | head -1 | grep -v \;);
-#   S=$(if grep :443 $x &> /dev/null; then echo SSL; fi);
-#   F=$(if grep MAGE_RUN $x &> /dev/null; then echo FIX; fi);
-#   if [[ $I != $V ]]; then color="${BRIGHT}${RED}"; else color="${GREY}"; fi;
-#   printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "$D";
-# done; echo
+ echo;
+ FMT=" %-15s  %-15s  %3s  %3s  %3s  %s\n"
+ HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %3s  %s${NORMAL}\n"
+ printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" "TMP" " Domain"
+ printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "---" "$(dash 44)"
+
+ for x in /etc/httpd/conf.d/vhost_[^000]*.conf; do
+   D=$(basename $x .conf | cut -d_ -f2);
+   V=$(awk '/.irtual.ost/ {print $2}' $x | head -1 | cut -d: -f1);
+   I=$(dig +short +time=1 +tries=1 $D | head -1 | grep -v \;);
+   S=$(if grep :443 $x &> /dev/null; then echo SSL; fi);
+   F=$(if grep MAGE_RUN $x &> /dev/null; then echo FIX; fi);
+   T=$(if [[ -n $(awk '/.irtual.ost/ {print $3}' $x &> /dev/null) ]]; then echo FIX; fi)
+   if [[ "$I" != "$V" ]];
+   then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "${T:- - }" "$D";
+   else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "${T:- - }" "$D"; fi
+ done; echo
 #
