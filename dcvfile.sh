e70f1a64872e92b5aac4bf2169695c218de73ccb
Mark Cunningham <mcunningham@nexcess.net>
2014-10-17 23:21:50 -0400

    Updated dcvfile.sh; Removed listemails.sh
    Streamlined dcvfile.sh to store modulus as variable and then process
    that through the hashing algorithm without having to process the CSR
    more than once.

--- dcvfile.sh
+++ dcvfile.sh
@@ -11,6 +11,10 @@
 # Create DCV txt file for domain validation when ordering an SSL
 
 if [[ -z $1 ]]; then read -p "Domain: " domain; else domain=$1; fi
-md5=$(openssl req -noout -modulus -in /home/*/var/${domain}/ssl/${domain}.csr | openssl md5 | awk '{print $2}' | sed 's/\(.*\)/\U\1/g');
-sha1=$(openssl req -noout -modulus -in /home/*/var/${domain}/ssl/${domain}.csr | openssl sha1 | awk '{print $2}' | sed 's/\(.*\)/\U\1/g');
-echo -e "${sha1}\ncomodoca.com" >> ${md5}.txt
+crtfile="/home/*/var/${domain}/ssl/${domain}.csr"
+if [[ -f $(echo $crtfile) ]]; then;
+mod="$(openssl req -noout -modulus -in $crtfile)"
+md5=$(echo $mod | openssl md5 | awk '{print $2}' | sed 's/\(.*\)/\U\1/g');
+sha1=$(echo $mod | openssl sha1 | awk '{print $2}' | sed 's/\(.*\)/\U\1/g');
+echo -e "${sha1}\ncomodoca.com" > ${md5}.txt
+else echo "Could not find csr for ${domain}!"; fi
--- listemails.sh
+++
@@ -1,1 +0,0 @@
-for x in /home/$(getusr)/var/*/mail/*/Maildir/; do echo $(echo $x | awk -F/ '{print $7"@"$5}'); done
