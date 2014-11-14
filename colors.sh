0abc96d993bf2a9a33a656cb72f6231bacf6f776
Mark Cunningham <mcunningham@nexcess.net>
2014-11-14 10:18:38 -0500

    Updated colors.sh
    Put a note for a for-loop showing all 256 colors

--- colors.sh
+++ colors.sh
@@ -1,3 +1,4 @@
+# Taste the rainbow
       BLACK=$(tput setaf 0);        RED=$(tput setaf 1)
       GREEN=$(tput setaf 2);     YELLOW=$(tput setaf 3)
        BLUE=$(tput setaf 4);    MAGENTA=$(tput setaf 5)
@@ -6,3 +7,6 @@
      BRIGHT=$(tput bold);        NORMAL=$(tput sgr0)
       BLINK=$(tput blink);      REVERSE=$(tput smso)
   UNDERLINE=$(tput smul)
+
+# Taste the 256 rainbow
+for i in {0..255} ; do printf "\x1b[38;5;${i}mcolour${i}\n"; done | column -x | less -R
