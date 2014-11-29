143a22b190ddbdcaaadee1731b0e132d39df3dd3
Mark Cunningham <mcunningham@nexcess.net>
2014-11-29 16:55:52 -0500

    Created brutecheck.sh
    This should work as a quick brute force or spamming check. Looks to see if
    there is a site getting over 999 post requests and what IPs are making more
    than 999 of those post requests. Would not work for DDoS, but would work
    for single IPs.

---
+++ brutecheck.sh
@@ -0,0 +1,30 @@
+#							   +----+----+----+----+
+# 							   |    |    |    |    |
+# Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
+# 							   +----+----+----+----+
+# Created: 2014-11-29
+# Updated: 2014-11-29
+#
+#
+#!/bin/bash
+
+# Check for site's being brute forced (can search for a particular request)
+
+echo; if [[ -n $1 ]]; then SEARCH="$1"; echo "Search: $SEARCH"; else read -p 'Search: ' SEARCH; fi; echo
+
+# Determin Log File Location
+#VHOST="/etc/httpd/conf.d/vhost_*.conf"
+#if [[ $(hostname) =~ .*-lb ]]; then
+#  LOGFILE="/var/log/interworx/*/*/logs/transfer.log";
+#  DOMAINS="$(echo $LOGFILE | cut -d/ -f6)"
+#else
+#  LOGFILE="$(awk '/CustomLog/ {print $2}' $VHOST | head -n1)";
+#  DOMAINS="$(echo $LOGFILE | cut -d/ -f5)";
+#fi
+
+for x in $(grep -Ec "POST.*${SEARCH}" /home/*/var/*/logs/transfer.log | grep -E [0-9]{4} | cut -d/ -f5); do
+  echo $x;
+  grep -E "POST.*${SEARCH}" /home/*/var/$x/logs/transfer.log\
+   | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}'\
+   | sort -rn | grep -E [0-9]{4};
+done; echo
