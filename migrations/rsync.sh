d40c8656af0b06e8fe3d762f9c70bbeb67d2ba4e
Mark Cunningham <mcunningham@nexcess.net>
2014-12-04 23:57:16 -0500

    Created new migrations scripts
    Updated rsync.sh to tar the var directory in order to be able to
    transfer it to the new account in toto then unpack on the other
    side with the importvar.sh script, which also runs varpermsfix

---
+++ migrations/rsync.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sitecopy(){
+
+USERNAME=$(pwd | sed 's:^/chroot::' | cut -d/ -f3)
+TARGET=$1
+LIVE=$2
+
+if [[ -z $1 ]]; then
+    echo "This script requires a hostname parameter."
+    return 1
+fi
+
+if [[ "$(pwd | sed 's:^/chroot::')" != "/home/$USERNAME" ]]; then
+    echo "You must run this from the user's home directory."
+    return 2
+fi
+
+if [[ -z $LIVE ]]; then
+    echo "PERFORMING DRY-RUN OF RSYNC"
+    sudo -u $USERNAME rsync -p --dry-run -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
+else
+    echo "Backing up var directory ..."; tar -czf var.tar.gz var/ && chown $USERNAME. var.tar.gz
+    echo "PERFORMING LIVE RSYNC"
+    sudo -u $USERNAME rsync --exclude ./var --exclude iworx-backup -p -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
+    echo "Cleaning up var backup ..."; rm var.tar.gz
+fi
+}
+
+sitecopy "$@"
