04d3eba6cf89694b658e297d0ba894bcbb4ed954
Mark Cunningham <mcunningham@nexcess.net>
2014-12-06 14:54:42 -0500

    Updated Migration scripts
    Updated rsync.sh to also backup all databases to a directory for copying
    Updated importvar.sh to import.sh and added database importing as well

--- migrations/import.sh
+++ migrations/import.sh
@@ -6,3 +6,9 @@ primaryDomain=$(~iworx/bin/listaccounts.pex | awk "/$(pwd | sed 's:^/chroot::' |
 echo "Runing varpermsfix on Siteworx account [$primaryDomain]"
 ~iworx/bin/varpermsfix.pex --siteworx=$primaryDomain
 echo "Cleaning up var backup ..."; rm -r var.old var.tar.gz; echo "Done"
+
+cd nex-db-backups
+for x in *.sql.gz; dbname="$(echo $x | cut -d. -f1)"
+  m -e"drop database $dbname; create database $dbname;"
+  zcat $x | pv -l "Importing $x" | m $dbname
+done && cd ../ && echo "Cleaning up nex-db-backups" && rm -r nex-db-backups
--- migrations/rsync.sh
+++ migrations/rsync.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+dbz(){
+  mysqldump --opt --skip-lock-tables -u "$(awk -F= '/user/ {print $2}' ~/.mytop)" -p"$(awk -F= '/pass/ {print $2}' ~/.mytop)" ${1} > ./nex-db-backups/${1}.$(date +%Y-%m-%d).sql.gz
+}
+
 sitecopy(){
 
 USERNAME=$(pwd | sed 's:^/chroot::' | cut -d/ -f3)
@@ -6,23 +10,34 @@ TARGET=$1
 LIVE=$2
 
 if [[ -z $1 ]]; then
-    echo "This script requires a hostname parameter."
-    return 1
+  echo "This script requires a hostname parameter."; return 1
 fi
 
 if [[ "$(pwd | sed 's:^/chroot::')" != "/home/$USERNAME" ]]; then
-    echo "You must run this from the user's home directory."
-    return 2
+  echo "You must run this from the user's home directory."
+  return 2
 fi
 
 if [[ -z $LIVE ]]; then
-    echo "PERFORMING DRY-RUN OF RSYNC"
-    sudo -u $USERNAME rsync -p --dry-run -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
+  echo "PERFORMING DRY-RUN OF RSYNC"
+  sudo -u $USERNAME rsync -p --dry-run -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
 else
-    echo "Backing up var directory ..."; tar -czf var.tar.gz var/ && chown $USERNAME. var.tar.gz
-    echo "PERFORMING LIVE RSYNC"
-    sudo -u $USERNAME rsync --exclude ./var --exclude iworx-backup -p -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
-    echo "Cleaning up var backup ..."; rm var.tar.gz
+  echo "Backing up var directory ...";
+    tar -czf var.tar.gz var/ && chown $USERNAME. var.tar.gz
+
+  echo "Backing up account databases ...";
+    if [[ ! -d nex-db-backups ]]; then mkdir nex-db-backups; fi
+    for x in /var/lib/mysql/${USERNAME}_*; do
+      echo "Backing up $x ..."
+      dbz $(echo $x | cut -d/ -f5)
+    done && chown -R ${USERNAME}. nex-db-backups
+
+  read -p "Press Enter to continue ..."
+
+  echo "PERFORMING LIVE RSYNC"
+  sudo -u $USERNAME rsync --exclude ./var --exclude iworx-backup -p -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
+
+  echo "Cleaning up var and db backups ..."; rm -r nex-db-backups var.tar.gz
 fi
 }
 
