dfbac9be406dc030efd2c314953938b9c8290033
Mark Cunningham <mcunningham@nexcess.net>
2014-12-13 19:44:43 -0500

    Updated freeslots.sh and freeslots()
    Added freeslots() to bashrc and updated regex matches on hostnames to be
    more robust in determining the type of server and the number of slots.

--- bashrc.sh
+++ bashrc.sh
@@ -516,9 +516,35 @@ done; echo
 
 ## Find IPs that are not configured in any vhost files
 freeips(){
-echo; for x in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.');
-do printf "\n%-15s " "$x"; grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf;
+echo; for x in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.'); do
+  printf "\n%-15s " "$x"; grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf;
 done | grep -v [a-z] | column -t; echo
+}
+
+## Calculate the free slots on a server depending on the server type
+freeslots(){
+echo "Working ..."
+nexaccts=$(nodeworx -unc Siteworx -a listAccounts | sed 's/ /_/g' | awk '($5 ~ /1/) {print $2}' | wc -l)
+resaccts=$(nodeworx -unc Siteworx -a listAccounts | sed 's/ /_/g' | awk '($5 !~ /1/) {print $2}' | wc -l)
+resellrs=$(nodeworx -unc Reseller -a listResellers | awk '($1 !~ /1/) {print $2}' | wc -l)
+
+echo -e "\nServer Admin Accounts\nSiteworx : $nexaccts"
+echo -e "\nReseller Accounts\nNodeworx : $resellrs\nSiteworx : $resaccts\n"
+
+if [[ $(hostname) =~ sip[a-z]*1-[0-9]* ]]; then echo "Free Slots: $(( 30 - $nexaccts ))"
+elif [[ $(hostname) =~ sip[a-z]*2-[0-9]* ]]; then echo "Free Slots: $(( 13 - $nexaccts ))"
+elif [[ $(hostname) =~ sip[a-z]*3-[0-9]* ]]; then echo "Free Slots: $(( 4 - $nexaccts ))"
+elif [[ $(hostname) =~ sipr-[0-9]* ]]; then echo "Free Slots: $(( 8 - $resellrs ))"
+elif [[ $(hostname) =~ (obp|eep|vbo)[a-z]*1-[0-9]* ]]; then echo "Free Slots: $(( 35 - $nexaccts ))"
+elif [[ $(hostname) =~ (opb|eep|vbo)[a-z]*2-[0-9]* ]]; then echo "Free Slots: $(( 15 - $nexaccts ))"
+elif [[ $(hostname) =~ (obp|eep|vbo)[a-z]*3-[0-9]* ]]; then echo "Free Slots: $(( 5 - $nexaccts ))"
+elif [[ $(hostname) =~ eepr-[0-9]* ]]; then echo "Free Slots: $(( 8 - $resellrs ))"; fi
+
+if [[ $(hostname) =~ ^sip ]]; then echo -e "\nFree IPAddresses";
+  for x in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.'); do
+    printf "\n%-15s " "$x"; grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf;
+  done | grep -v [a-z] | column -t;
+else echo "These do not need dedicated IPs"; fi; echo
 }
 
 ## Check if gzip is working for domain(s)
--- freeslots.sh
+++ freeslots.sh
@@ -1,42 +1,31 @@
+#							   +----+----+----+----+
+# 							   |    |    |    |    |
+# Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
+# 							   +----+----+----+----+
+# Created: 2014-12-06
+# Updated: 2014-12-13
+#
+#
+#!/bin/bash
+
 echo "Working ..."
 nexaccts=$(nodeworx -unc Siteworx -a listAccounts | sed 's/ /_/g' | awk '($5 ~ /1/) {print $2}' | wc -l)
 resaccts=$(nodeworx -unc Siteworx -a listAccounts | sed 's/ /_/g' | awk '($5 !~ /1/) {print $2}' | wc -l)
-totaccts=$(~iworx/bin/listaccounts.pex | wc -l)
 resellrs=$(nodeworx -unc Reseller -a listResellers | awk '($1 !~ /1/) {print $2}' | wc -l)
 
-echo -e "\nServer Admin Accounts"
-echo "Siteworx : $nexaccts"
+echo -e "\nServer Admin Accounts\nSiteworx : $nexaccts"
+echo -e "\nReseller Accounts\nNodeworx : $resellrs\nSiteworx : $resaccts\n"
 
-echo -e "\nReseller Accounts"
-echo "Nodeworx : $resellrs"
-echo "Siteworx : $resaccts"
+if [[ $(hostname) =~ sip[a-z]*1-[0-9]* ]]; then echo "Free Slots: $(( 30 - $nexaccts ))"
+elif [[ $(hostname) =~ sip[a-z]*2-[0-9]* ]]; then echo "Free Slots: $(( 13 - $nexaccts ))"
+elif [[ $(hostname) =~ sip[a-z]*3-[0-9]* ]]; then echo "Free Slots: $(( 4 - $nexaccts ))"
+elif [[ $(hostname) =~ sipr-[0-9]* ]]; then echo "Free Slots: $(( 8 - $resellrs ))"
+elif [[ $(hostname) =~ (obp|eep|vbo)[a-z]*1-[0-9]* ]]; then echo "Free Slots: $(( 35 - $nexaccts ))"
+elif [[ $(hostname) =~ (opb|eep|vbo)[a-z]*2-[0-9]* ]]; then echo "Free Slots: $(( 15 - $nexaccts ))"
+elif [[ $(hostname) =~ (obp|eep|vbo)[a-z]*3-[0-9]* ]]; then echo "Free Slots: $(( 5 - $nexaccts ))"
+elif [[ $(hostname) =~ eepr-[0-9]* ]]; then echo "Free Slots: $(( 8 - $resellrs ))"; fi
 
-#echo -e "\nTotal Accounts"
-#echo "Siteworx : $totaccts"
-
-echo
-if [[ $(hostname) =~ sip.*1- ]]; then
-  echo "Free Slots: $(( 30 - $nexaccts ))"
-elif [[ $(hostname) =~ sip.*2- ]]; then
-  echo "Free Slots: $(( 13 - $nexaccts ))"
-elif [[ $(hostname) =~ sip.*3- ]]; then
-  echo "Free Slots: $(( 4 - $nexaccts ))"
-elif [[ $(hostname) =~ sipr- ]]; then
-  echo "Free Slots: $(( 8 - $resellrs ))"
-
-elif [[ $(hostname) =~ (obp|eep|vbo).*1- ]]; then
-  echo "Free Slots: $(( 35 - $nexaccts ))"
-elif [[ $(hostname) =~ (opb|eep|vbo).*2- ]]; then
-  echo "Free Slots: $(( 15 - $nexaccts ))"
-elif [[ $(hostname) =~ (obp|eep|vbo).*3- ]]; then
-  echo "Free Slots: $(( 5 - $nexaccts ))"
-elif [[ $(hostname) =~ eepr- ]]; then
-  echo "Free Slots: $(( 8 - $resellrs ))"
-
-fi
-
-if [[ $(hostname) =~ ^sip ]]; then
-  echo -e "\nFree IPAddresses";
+if [[ $(hostname) =~ ^sip ]]; then echo -e "\nFree IPAddresses";
   for x in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.'); do
     printf "\n%-15s " "$x"; grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf;
   done | grep -v [a-z] | column -t;
