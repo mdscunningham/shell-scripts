455ad0d97c292cda2393574a7787b405d9957105
Mark Cunningham <mcunningham@nexcess.net>
2014-12-21 22:18:39 -0500

    Updated domaincheck.sh / bashrc.sh -- domaincheck()
    Combined accountips and domaincheck and resellerchk all into domaincheck
    accountips -> 'domaincheck -a [subdomain]'
    resellerchk -> 'domaincheck -r [resellerID]'

--- bashrc.sh
+++ bashrc.sh
@@ -483,42 +483,42 @@ echo; for I in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -E
 done; echo
 }
 
-## Find IPs in use by account by finding secondary domains, and searching the vhost files
-accountips(){
-    echo; DIR=$PWD; cd /home/$(getusr);
-    FORMAT=" %-15s  %-15s  %3s  %3s  %s\n";
-    HIGHLIGHT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %s${NORMAL}\n";
-    printf "$FORMAT" " ServerIP" " LiveIP" "SSL" "FPM" " DomainName";
-    printf "$FORMAT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash 39)";
-    for x in */html; do
-        D=$(echo $x | cut -d/ -f1);
-	vhost="/etc/httpd/conf.d/vhost_$D.conf"
-        L=$(dig +tries=1 +time=3 +short $1$D | grep -E '^[0-9]{1,3}\.' | head -n1);
-        I=$(awk '/.irtual.ost/ {print $2}' $vhost 2> /dev/null | head -n1 | cut -d: -f1);
-        S=$(if grep -q ':443' $vhost &> /dev/null; then echo SSL; fi)
-	F=$(if grep -q 'MAGE_RUN' $vhost &> /dev/null; then echo FIX; fi)
-        if [[ $I != $L ]];
-	  then printf "$HIGHLIGHT" "$I" "$L" "${S:- - }" "${F:- - }" "$1$D";
-          else printf "$FORMAT" "$I" "$L" "${S:- - }" "${F:- - }" "$1$D"; fi;
-    done;
-    echo; cd $DIR
-}
+## Find IPs in use by a Siteowrx account
+accountips(){ domaincheck -a $1; }
+
+## Find IPs in use by a Reseller account
+resellerips(){ domaincheck -r $1; }
 
 ## Match secondary domains to IPs on the server using vhost files
 domaincheck(){
- echo; FMT=" %-15s  %-15s  %3s  %3s  %s\n"
- HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %s${NORMAL}\n"
- printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" " Domain"
- printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash 44)"
- for x in /etc/httpd/conf.d/vhost_[^000]*.conf; do
+vhost="$(echo /etc/httpd/conf.d/vhost_[^000]*.conf)"; sub='';
+
+case $1 in
+  -a) if [[ -n $2 ]]; then sub=$2; else sub=''; fi
+      vhost="$(grep -l $(getusr) /etc/httpd/conf.d/vhost_[^000]*.conf)" ;;
+  -r) if [[ -z $2 ]]; then read -p "ResellerID: " r_id; else r_id=$2; fi; echo;
+      vhost=$(for r_user in $(nodeworx -unc Siteworx -a listAccounts | awk "(\$5 ~ /^$r_id$/)"'{print $2}'); do grep -l $r_user /etc/httpd/conf.d/vhost_[^000]*.conf; done | sort | uniq) ;;
+  -v) FMT=" %-15s  %-15s  %3s  %3s  %3s  %s\n"
+      HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %3s  %s${NORMAL}\n"
+      #printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" "TMP" " Domain"
+      #printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "---" "$(dash 44)"
+      ;;
+esac
+
+FMT=" %-15s  %-15s  %3s  %3s  %s\n"
+HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %s${NORMAL}\n"
+printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" " Domain"
+printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash 44)"
+
+for x in $vhost; do
    D=$(basename $x .conf | cut -d_ -f2);
    V=$(awk '/.irtual.ost/ {print $2}' $x | head -1 | cut -d: -f1);
-   I=$(dig +short +time=1 +tries=1 $D | grep -E '^[0-9]{1,3}\.' | head -1);
+   I=$(dig +short +time=1 +tries=1 ${sub}$D | grep -E '^[0-9]{1,3}\.' | head -1);
    S=$(if grep :443 $x &> /dev/null; then echo SSL; fi);
    F=$(if grep MAGE_RUN $x &> /dev/null; then echo FIX; fi);
    if [[ "$I" != "$V" ]];
-   then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "$D";
-   else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "$D"; fi
+   then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "${sub}$D";
+   else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "${sub}$D"; fi
  done; echo
 }
 
--- domaincheck.sh
+++ domaincheck.sh
@@ -3,7 +3,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2013-11-18
-# Updated: 2014-12-17
+# Updated: 2014-12-21
 #
 #
 #!/bin/bash
@@ -11,55 +11,39 @@
 #source colors.sh
 dash(){ for ((i=1; i<=$1; i++)); do printf "-"; done; }
 
-# echo;
-# FORMAT=" %-15s  %-15s  %-3s  %s\n";
-# HIGHLIGHT="${BRIGHT}${RED} %-15s  %-15s  %-3s  %s${NORMAL}\n";
-# printf "$FORMAT" " ServerIP" " LiveIP" "SSL" " DomainName";
-# printf "$FORMAT" "$(dash 15)" "$(dash 15)" "---" "$(dash 44)";
-# for x in /etc/httpd/conf.d/vhost_[^000_]*.conf;
-# do
-#    D=$(basename $x .conf | cut -d_ -f2);
-#    I=$(grep -i virtualhost $x 2> /dev/null | head -n1 | awk '{print $2}' | cut -d: -f1);
-#    L=$(dig +time=3 +tries=1 +short $D | grep -v \; | head -n1);
-#    if grep ':443' $x &> /dev/null; then
-#        S="YES";
-#    else
-#        S="NO";
-#    fi;
-#    if [[ $I != $L ]]; then
-#        printf "$HIGHLIGHT" "$I" "$L" "$S" "$D";
-#    else
-#        printf "$FORMAT" "$I" "$L" "$S" "$D";
-#    fi;
-# done;
-# echo
+domaincheck(){
+vhost="$(echo /etc/httpd/conf.d/vhost_[^000]*.conf)"; sub='';
 
-#
- echo;
-# FMT=" %-15s  %-15s  %3s  %3s  %3s  %s\n"
-# HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %3s  %s${NORMAL}\n"
-# printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" "TMP" " Domain"
-# printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "---" "$(dash 44)"
+case $1 in
+  -a) if [[ -n $2 ]]; then sub=$2; else sub=''; fi
+      vhost="$(grep -l $(getusr) /etc/httpd/conf.d/vhost_[^000]*.conf)" ;;
+  -r) if [[ -z $2 ]]; then read -p "ResellerID: " r_id; else r_id=$2; fi; echo;
+      vhost=$(for r_user in $(nodeworx -unc Siteworx -a listAccounts | awk "(\$5 ~ /^$r_id$/)"'{print $2}'); do grep -l $r_user /etc/httpd/conf.d/vhost_[^000]*.conf; done | sort | uniq) ;;
+  -v) FMT=" %-15s  %-15s  %3s  %3s  %3s  %s\n"
+      HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %3s  %s${NORMAL}\n"
+      #printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" "TMP" " Domain"
+      #printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "---" "$(dash 44)"
+      ;;
+esac
 
- FMT=" %-15s  %-15s  %3s  %3s  %s\n"
- HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %s${NORMAL}\n"
- printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" " Domain"
- printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash 44)"
+FMT=" %-15s  %-15s  %3s  %3s  %s\n"
+HLT="${BRIGHT}${RED} %-15s  %-15s  %3s  %3s  %s${NORMAL}\n"
+printf "$FMT" " Server IP" " Live IP" "SSL" "FPM" " Domain"
+printf "$FMT" "$(dash 15)" "$(dash 15)" "---" "---" "$(dash 44)"
 
- for x in /etc/httpd/conf.d/vhost_[^000]*.conf; do
-   D=$(basename $x .conf | cut -d_ -f2);
-   V=$(awk '/.irtual.ost/ {print $2}' $x | head -1 | cut -d: -f1);
-   I=$(dig +short +time=1 +tries=1 $D | grep -E '^[0-9]{1,3}\.' | head -1);
-   S=$(if grep :443 $x &> /dev/null; then echo SSL; fi);
-   F=$(if grep MAGE_RUN $x &> /dev/null; then echo FIX; fi);
-   # T=$(if [[ -n $(awk '/.irtual.ost/ {print $3}' $x) ]]; then echo FIX; fi)
-   if [[ "$I" != "$V" ]];
-   # then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "${T:- - }" "$D";
-   then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "$D";
-   # else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "${T:- - }" "$D"; fi
-   else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "$D"; fi
- done; echo
-#
+for x in $vhost; do
+  D=$(basename $x .conf | cut -d_ -f2);
+  V=$(awk '/.irtual.ost/ {print $2}' $x | head -1 | cut -d: -f1);
+  I=$(dig +short +time=1 +tries=1 ${sub}$D | grep -E '^[0-9]{1,3}\.' | head -1);
+  S=$(if grep :443 $x &> /dev/null; then echo SSL; fi);
+  F=$(if grep MAGE_RUN $x &> /dev/null; then echo FIX; fi);
+  T=$(if [[ -n $(awk '/.irtual.ost/ {print $3}' $x) ]]; then echo FIX; fi)
+  if [[ "$I" != "$V" ]];
+  # then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "${T:- - }" "${sub}$D";
+  then printf "$HLT" "$V" "$I" "${S:- - }" "${F:- - }" "${sub}$D";
+  # else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "${T:- - }" "${sub}$D"; fi
+  else printf "$FMT" "$V" "$I" "${S:- - }" "${F:- - }" "${sub}$D"; fi
+done; echo
+}
 
-# do for a reseller's accounts
-# for x in $(lreseller | awk '($1 ~ /4/) && !($2 ~ /0/) {print $3}'); do echo -n $(dig +short $x); echo " -> $x"; done | column -t
+domaincheck "$@"
