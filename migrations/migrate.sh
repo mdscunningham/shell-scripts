172cabba1e25e4757c24cfb379e3c47a93544556
Mark Cunningham <mcunningham@nexcess.net>
2014-12-23 23:38:01 -0500

    Updated migrations/migrate.sh
    Added whiteliseting for remote mysql and use siteworx to list databases

--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -5,16 +5,21 @@ dbz(){ DBUSER="$(awk -F= '/user/ {print $2}' /root/.mytop)"; DBPASS="$(awk -F= '
 sitecopy(){
 USERNAME="$(pwd | sed 's:^/chroot::' | cut -d/ -f3)"
 local OPTIND
-while getopts t:dlvhs option; do
+while getopts w:t:dlvhs option; do
   case "${option}" in
     t) TARGET="${OPTARG}" ;;
     l) LIVE='1' ;;
-    d) echo; mkdir nex-db-backups; for x in /var/lib/mysql/${USERNAME}_*; do echo "Backing up $x ..."; dbz $(echo "$x" | cut -d/ -f5); done && chown -R ${USERNAME}. nex-db-backups ;;
+    d) echo; mkdir nex-db-backups;
+       for x in $(sudo -u $USERNAME siteworx -unc MysqlDb -a list | awk '{print $2}'); do
+         echo "Backing up $x ..."; dbz $(echo "$x" | cut -d/ -f5);
+       done && chown -R ${USERNAME}. nex-db-backups ;;
     v) echo -e "\nBacking up var directory ..."; tar -czpf var.tar.gz var/ && chown $USERNAME. var.tar.gz ;;
-    s) ~iworx/bin/backup.pex --structure-only --domains=$(~iworx/bin/listaccounts.pex | awk  "/$USERNAME/"'{print $2}') --output-dir=$(pwd) ;;
+    w) REMOTE="${OPTARG}"; echo -e "\n# $USERNAME (Mysql to $REMOTE)\nd=3306:d=$(dig +short $REMOTE)" | tee -a /etc/apf/allow_hosts.rules; apf -r ;;
+    s) ~iworx/bin/backup.pex --structure-only --domains=$(~iworx/bin/listaccounts.pex | awk  "(\$1 ~ /$USERNAME/)"'{print $2}') --output-dir=$(pwd) ;;
     h) echo -e "\n  Usage: ./migrate.sh [options]
     -s .. create structure backup
     -t <host> .. target server name
+    -w <host> .. whitelist for mysql out
     -d .. backup databases
     -v .. backup var directory
     -l .. live transfer
@@ -28,10 +33,10 @@ if [[ $LIVE != '1' && -n $TARGET ]]; then
 elif [[ $LIVE == '1' && -n $TARGET ]]; then
   echo -e "PERFORMING LIVE RSYNC\n"
   sudo -u $USERNAME rsync --exclude ./var --exclude iworx-backup -p -v -e ssh -a -u -z ./ $USERNAME@$TARGET:/home/$USERNAME
-  echo -e "Cleaning up var and db backups ...\n"; rm -r nex-db-backups var.tar.gz; echo -e "Done!\n"
+  echo -e "Cleaning up var and db backups ...\n"; rm -r nex-db-backups var.tar.gz 2> /dev/null; echo -e "Done!\n"
 fi
 
-unset USERNAME TARGET LIVE
+unset USERNAME TARGET REMOTE LIVE
 }
 
 sitecopy "$@"
