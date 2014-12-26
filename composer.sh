48007fd1c039a3d92c110d7d1161a96918452a59
Mark Cunningham <mcunningham@nexcess.net>
2014-12-26 16:23:48 -0500

    Updated composer.sh
    Corrected alias listed for composer.phar

--- composer.sh
+++ composer.sh
@@ -1,6 +1,6 @@
 # Install Composer
 sudo -u $(getusr) -- curl -sS https://getcomposer.org/installer | php -d allow_url_fopen=On -d apc.enable_cli=Off
-alias composer="php -d -d allow_url_fopen=On -d disable_functions='' -open_basedir='' ~/composer.phar"
+alias composer="php -d allow_url_fopen=On -d disable_functions='' -d open_basedir='' ~/composer.phar"
 
 # Install WP-CLI
 sudo -u $(getusr) -- curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
