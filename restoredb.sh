b36d4b15ae5456af2f6abf9ae461e0792c18fe2e
Mark Cunningham <mcunningham@nexcess.net>
2015-01-09 14:11:53 -0500

    Updated restoredb.sh and restoredb()
    Corrected for updating ownership of the datbase files after the import
    ensuring that the database counts toward their disk quota

--- restoredb.sh
+++ restoredb.sh
@@ -14,11 +14,13 @@ DBNAME=$(echo $DBFILE | cut -d. -f1);
 echo "Creating current $DBNAME backup ..."; mdz $DBNAME;
 echo "Dropping current $DBNAME ..."; m -e"drop database $DBNAME";
 echo "Creating empty $DBNAME ..."; m -e"create database $DBNAME";
-if [[ -d /home/mysql/$DBNAME/ ]]; then echo "Fixing Ownership on new DB ..."; chown -R mysql:$(echo $DBNAME | cut -d_ -f1) /home/mysql/$DBNAME/; fi
 echo "Importing backup $DBFILE ...";
 
 if [[ $DBFILE =~ \.gz$ ]]; then SIZE=$(gzip -l $DBFILE | awk 'END {print $2}');
 elif [[ $DBFILE =~ \.zip$ ]]; then SIZE=$(unzip -l $DBFILE | awk '{print $1}'); fi
 
 if [[ -f /usr/bin/pv ]]; then zcat -f $DBFILE | pv -s $SIZE | m $DBNAME;
-else zcat -f $DBFILE | m $DBNAME; fi; echo
+else zcat -f $DBFILE | m $DBNAME; fi;
+
+if [[ -d /home/mysql/$DBNAME/ ]]; then echo "Fixing Ownership on new DB ..."; chown -R mysql:$(echo $DBNAME | cut -d_ -f1) /home/mysql/$DBNAME/; fi
+echo
