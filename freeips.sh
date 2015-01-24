6609cf40479080c2053b7f6b087b0a3d5693fdbe
Mark Cunningham <mcunningham@nexcess.net>
2015-01-24 09:56:00 -0500

    Updated freeips.sh freeslots.sh
    Now directs grep 2>/dev/null when it can't find any vhosts on a new server

--- freeips.sh
+++ freeips.sh
@@ -3,7 +3,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2013-11-29
-# Updated: 2014-07-28
+# Updated: 2015-01-23
 #
 #
 #!/bin/bash
@@ -14,7 +14,7 @@ _freeips ()
     for x in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.');
     do
         printf "\n%-15s " "$x";
-        grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf;
+        grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf 2> /dev/null;
     done | grep -v .conf$ | column -t;
     echo
 }
--- freeslots.sh
+++ freeslots.sh
@@ -3,7 +3,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-12-06
-# Updated: 2014-12-13
+# Updated: 2015-01-23
 #
 #
 #!/bin/bash
@@ -27,8 +27,8 @@ elif [[ $(hostname) =~ eepr-[0-9]* ]]; then echo "Free Slots: $(( 8 - $resellrs 
 
 if [[ $(hostname) =~ ^sip ]]; then echo -e "\nFree IPAddresses";
   for x in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.'); do
-    printf "\n%-15s " "$x"; grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf;
-  done | grep -v [a-z] | column -t;
+    printf "\n%-15s " "$x"; grep -l $x /etc/httpd/conf.d/vhost_[^000_]*.conf 2> /dev/null;
+  done | grep -v .conf$ | column -t;
 else echo "These do not need dedicated IPs"; fi; echo
 
 
