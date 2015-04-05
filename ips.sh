d87f589059e68375e4b418d5e2085be018fe2351
root <root@host.axeblade.net>
2015-04-05 18:09:36 -0400

    Merge in all cpanel conversion updates

---
+++ ips.sh
@@ -0,0 +1,19 @@
+#							   +----+----+----+----+
+# 							   |    |    |    |    |
+# Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
+# 							   +----+----+----+----+
+# Created: 2014-05-03
+# Updated: 2014-05-03
+#
+#
+#!/bin/bash
+
+#echo;
+#macaddr=($(ifconfig | awk '/^e/ {print $1"_"$5}'))
+#ipaddr=($(ifconfig | awk -F: '/\inet addr/ {print $2}' | awk '{print $1}' | grep -Ev '^127\.|^10\.|^172\.'))
+
+#(echo "NIC MAC-Address IP-Address"
+#for ((i=0;i<${#ipaddr[@]};i++)); do echo "${macaddr[i]} ${ipaddr[i]}" | sed 's/_/ /g'; done) | column -t
+#echo
+
+ip addr show | awk '/inet / && !/127\./ {print $2}' | cut -d/ -f1
---
+++ mail-test.sh
@@ -0,0 +1,1 @@
+mail-check.sh
