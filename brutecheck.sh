0018e94fe35ad8c32cd4aa0a0e3e44b62f965d1d
mdscunningham <mcunningham@mdsc.info>
2015-04-19 16:27:31 -0400

    Updated brutecheck.sh to work for cPanel logs, tested and working.

--- brutecheck.sh
+++ brutecheck.sh
@@ -3,16 +3,17 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-11-29
-# Updated: 2015-04-01
+# Updated: 2015-04-19
 #
 #
 #!/bin/bash
 
 # Check for site's being brute forced (can search for a particular request)
+shopt -s extglob
 
 echo; if [[ -n $1 ]]; then SEARCH="$1"; echo "Search: $SEARCH"; else read -p 'Search: ' SEARCH; fi; echo
-for x in $(grep -Ec "POST.*${SEARCH}" /usr/local/apache/domlogs/*/*[^_log] 2> /dev/null | grep -E [0-9]{4}$ | cut -d/ -f7); do
-  echo $x; ./traffic.sh $x ip -s POST.*${SEARCH} | grep -E [0-9]{4}; echo;
+for x in $(grep -Ec "POST.*${SEARCH}" /usr/local/apache/domlogs/*/*[^_log] 2> /dev/null | grep -E [0-9]{4}$ | cut -d/ -f7 | cut -d: -f1); do
+  echo $x; grep "$SEARCH" /usr/local/apache/domlogs/*/$x | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}' | grep -E [0-9]{4}; echo;
 done
 
 # Determin Log File Location
