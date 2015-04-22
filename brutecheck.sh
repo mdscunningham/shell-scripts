b188978d6db988e061746b8c77a55a3e81aed3da
mdscunningham <mcunningham@mdsc.info>
2015-04-21 21:50:23 -0400

    Removed old comments from brutecheck.sh

--- brutecheck.sh
+++ brutecheck.sh
@@ -3,7 +3,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-11-29
-# Updated: 2015-04-19
+# Updated: 2015-04-21
 #
 #
 #!/bin/bash
@@ -15,20 +15,3 @@ echo; if [[ -n $1 ]]; then SEARCH="$1"; echo "Search: $SEARCH"; else read -p 'Se
 for x in $(grep -Ec "POST.*${SEARCH}" /usr/local/apache/domlogs/*/*[^_log] 2> /dev/null | grep -E [0-9]{4}$ | cut -d/ -f7 | cut -d: -f1); do
   echo $x; grep "$SEARCH" /usr/local/apache/domlogs/*/$x | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}' | grep -E [0-9]{4}; echo;
 done
-
-# Determin Log File Location
-#VHOST="/etc/httpd/conf.d/vhost_*.conf"
-#if [[ $(hostname) =~ .*-lb ]]; then
-#  LOGFILE="/var/log/interworx/*/*/logs/transfer.log";
-#  DOMAINS="$(echo $LOGFILE | cut -d/ -f6)"
-#else
-#  LOGFILE="$(awk '/CustomLog/ {print $2}' $VHOST | head -n1)";
-#  DOMAINS="$(echo $LOGFILE | cut -d/ -f5)";
-#fi
-
-#for x in $(grep -Ec "POST.*${SEARCH}" /home/*/var/*/logs/transfer.log | grep -E [0-9]{4} | cut -d/ -f5); do
-#  echo $x;
-#  grep -E "POST.*${SEARCH}" /home/*/var/$x/logs/transfer.log\
-#   | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}'\
-#   | sort -rn | grep -E [0-9]{4}; echo
-#done;
