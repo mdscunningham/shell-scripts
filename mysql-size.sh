296d2eb50b6e50a561d41765cef4bf61052048e5
mdscunningham <mcunningham@mdsc.info>
2015-05-02 03:53:17 -0400

    Updated mysql-size.sh
    Removed lookup of iworx mysql password, now assumes root privledges

--- mysql-size.sh
+++ mysql-size.sh
@@ -9,27 +9,27 @@
 #!/bin/bash
 
 ## Get root database user credentials from server configuration
-dbuser=$(grep ^rootdsn= /home/interworx/iworx.ini | cut -d/ -f3 | cut -d: -f1);
-dbpass=$(grep ^rootdsn= /home/interworx/iworx.ini | cut -d: -f3 | cut -d\@ -f1);
+#dbuser=$(grep ^rootdsn= /home/interworx/iworx.ini | cut -d/ -f3 | cut -d: -f1);
+#dbpass=$(grep ^rootdsn= /home/interworx/iworx.ini | cut -d: -f3 | cut -d\@ -f1);
 dbhost="localhost";
 
 ## Setup headers for formatted output.
 echo;
-printf "%-11s  %-11s  %-11s  %-11s  %s\n" " On Disk   " " DataIndex " " Diff      " " DataFree  " " Database Name  "
+printf "%-11s  %-11s  %-11s  %-11s  %s\n" " On Disk   " " MySQLData " " Size Diff " " DataFree  " " Database Name  "
 printf "%-11s  %-11s  %-11s  %-11s  %s\n" "-----------" "-----------" "-----------" "-----------" "---------------------------------------------------"
 
 ## Get list of databases
-databases=$(mysql -h "$dbhost" -u "$dbuser" -p"$dbpass" --batch --skip-column-names -e 'show databases' | grep -v '^performance_schema$')
+databases=$(mysql -h "$dbhost" --batch --skip-column-names -e 'show databases' | grep -v '^performance_schema$')
 
 for x in $databases; do
     ## Get size on disk and size of data in the tables
 	disksize=$(du -sb /var/lib/mysql/$x 2> /dev/null | awk '{print $1}');
 	if [[ $disksize == '' ]]; then disksize=0; fi
 
-	dbsize=$(mysql -u$dbuser -p$dbpass -h$dbhost -e"select SUM(Data_length + Index_length) FROM information_schema.TABLES WHERE TABLE_SCHEMA = \"$x\";" | tail -n1);
+	dbsize=$(mysql -h$dbhost -e"select SUM(Data_length + Index_length) FROM information_schema.TABLES WHERE TABLE_SCHEMA = \"$x\";" | tail -n1);
 	if [[ $dbsize == 'NULL' ]]; then dbsize=0; fi
 
-	free=$(mysql -u$dbuser -p$dbpass -h$dbhost -e"select SUM(Data_free) FROM information_schema.TABLES WHERE TABLE_SCHEMA = \"$x\";" | tail -n1);
+	free=$(mysql -h$dbhost -e"select SUM(Data_free) FROM information_schema.TABLES WHERE TABLE_SCHEMA = \"$x\";" | tail -n1);
         if [[ $free == 'NULL' ]]; then free=0; fi
 
     ## Calculate Megabytes from bytes
