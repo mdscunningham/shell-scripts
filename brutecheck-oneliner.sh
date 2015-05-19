6d1f8caaa3c01b194f0478100aed1557336bf4ce
mdscunningham <mcunningham@mdsc.info>
2015-05-18 20:46:19 -0400

    Updated brutecheck scripts
    * Fixed a grep to grep -E for looking up regex like 'this|that'
    * Changed min display from 4 digits to 3 digits worth of posts

--- brutecheck-oneliner.sh
+++ brutecheck-oneliner.sh
@@ -9,4 +9,4 @@
 #!/bin/bash
 
 # Check for site's being brute forced (can search for a particular request)
-shopt -s extglob; echo; read -p 'Search: ' SEARCH; echo; for x in $(grep -Ec "POST.*${SEARCH}" /usr/local/apache/domlogs/*/*[^_log] 2> /dev/null | grep -E [0-9]{4}$ | cut -d/ -f7 | cut -d: -f1); do echo $x; grep "$SEARCH" /usr/local/apache/domlogs/*/$x | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}' | grep -E [0-9]{4}; echo; done
+shopt -s extglob; echo; read -p 'Search: ' SEARCH; echo; for x in $(grep -Ec "POST.*${SEARCH}" /usr/local/apache/domlogs/*/*[^_log] 2> /dev/null | grep -E [0-9]{4}$ | cut -d/ -f7 | cut -d: -f1); do echo $x; grep -E "$SEARCH" /usr/local/apache/domlogs/*/$x | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}' | grep -E '[0-9]{3}\ '; echo; done
--- brutecheck.sh
+++ brutecheck.sh
@@ -13,5 +13,5 @@ shopt -s extglob
 
 echo; if [[ -n $1 ]]; then SEARCH="$1"; echo "Search: $SEARCH"; else read -p 'Search: ' SEARCH; fi; echo
 for x in $(grep -Ec "POST.*${SEARCH}" /usr/local/apache/domlogs/*/*[^_log] 2> /dev/null | grep -E [0-9]{4}$ | awk -F/ '{print $NF}' | cut -d: -f1); do
-  echo $x; grep "$SEARCH" /usr/local/apache/domlogs/*/$x | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}' | grep -E '[0-9]{3}\ '; echo;
+  echo $x; grep -E "$SEARCH" /usr/local/apache/domlogs/*/$x | awk '{freq[$1]++} END {for (x in freq) {printf "%8s %s\n",freq[x],x}}' | grep -E '[0-9]{3}\ '; echo;
 done
