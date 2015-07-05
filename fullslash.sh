7067b09c270a57f47e7e0353a607b6149ca2c6f3
mdscunningham <mcunningham@mdsc.info>
2015-07-05 17:18:12 -0400

    Updated fullslash.sh
    * Corrected awk to print the start of the partition name rather than the end

--- fullslash.sh
+++ fullslash.sh
@@ -13,7 +13,7 @@
 shopt -s extglob
 
 # Generate list of partitions to exlude
-PARTLIST=$(df -h | awk -F/ 'NR>2 {printf "%s|",$NF}' | sed 's/|$/|proc/g';)
+PARTLIST=$(df -h | awk -F/ 'NR>2 {printf "%s|",$2}' | sed 's/|$/|proc/g';)
 
 # Summarize disk usage for all non-partition directories in '/'
 du --max-depth 3 -h /!($PARTLIST) | grep --color -E '^[0-9]{3}M|^[0-9\.]*G'
@@ -21,6 +21,6 @@ du --max-depth 3 -h /!($PARTLIST) | grep --color -E '^[0-9]{3}M|^[0-9\.]*G'
 
 ## Oneliner-version
 #
-# shopt -s extglob; du --max-depth 3 -h /!($(df -h | awk -F/ 'NR>2 {printf "%s|",$NF}' | sed 's/|$/|proc/g';)) | grep --color -E '^[0-9]{3}M|^[0-9\.]*G'
+# shopt -s extglob; du --max-depth 3 -h /!($(df -h | awk -F/ 'NR>2 {printf "%s|",$2}' | sed 's/|$/|proc/g';)) | grep --color -E '^[0-9]{3}M|^[0-9\.]*G'
 #
 ##
