ef61ed3c4830a5b18c215639f8a7e666f69fe6db
mdscunningham <mcunningham@mdsc.info>
2015-07-06 17:26:38 -0400

    Updated magdb.sh
    * Added colors definitions, and getusr() to make it more stand-alone
    * Need to update the mysql queries to 2>/dev/null (hide password used at CLI message)

--- magdb.sh
+++ magdb.sh
@@ -14,6 +14,18 @@
 ## https://dev.mysql.com/doc/refman/5.6/en/show-table-status.html
 ## ^^^ much faster way to get rows, data, index, data_free if necessary
 
+# Taste the rainbow
+      BLACK=$(tput setaf 0);        RED=$(tput setaf 1)
+      GREEN=$(tput setaf 2);     YELLOW=$(tput setaf 3)
+       BLUE=$(tput setaf 4);     PURPLE=$(tput setaf 5)
+       CYAN=$(tput setaf 6);      WHITE=$(tput setaf 7)
+
+     BRIGHT=$(tput bold);        NORMAL=$(tput sgr0)
+      BLINK=$(tput blink);      REVERSE=$(tput smso)
+  UNDERLINE=$(tput smul)
+
+getusr(){ pwd | sed 's:^/chroot::' | cut -d/ -f3; }
+
 echo; runonce=0;
 if [[ $1 =~ ^-.*$ ]]; then SITEPATH='.'; opt="$1"; shift; param="$@";
 else SITEPATH="$1"; opt="$2"; shift; shift; param="$@"; fi;
