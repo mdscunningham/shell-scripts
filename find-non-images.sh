f9d342799e7e5dc8a29adfb3e7fc00a5c72f447b
mdscunningham <mcunningham@mdsc.info>
2015-07-27 23:56:38 -0400

    Added find-non-images.sh
    * Searches public_html directories for what look like image files but are actully php

---
+++ find-non-images.sh
@@ -0,0 +1,14 @@
+#							   +----+----+----+----+
+# 							   |    |    |    |    |
+# Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
+# 							   +----+----+----+----+
+# Created: 2015-06-23
+# Updated: 2015-07-27
+#
+#
+#!/bin/bash
+
+# So when using regex matching in find, you need to escape both the () and |
+# http://stackoverflow.com/questions/19111067/regex-match-either-string-in-linux-find-command
+
+find /home/*/public_html/ -type f -regex ".*\(jpg\|jpeg\|gif\|png\|bmp\)$" -print0 | xargs -0 grep -l '<?php' 2> /dev/null
