78429651c0e7ea9a853cd7c9c9a0cfb7192f205a
mdscunningham <mcunningham@mdsc.info>
2015-08-16 21:49:30 -0400

    Updated modsec.sh
    * Added date output when using a specified date (-d <days>)
    * Added a domain search option (-D <domain>)

--- modsec.sh
+++ modsec.sh
@@ -3,7 +3,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-01-01
-# Updated: 2015-05-20
+# Updated: 2015-08-16
 #
 #
 #!/bin/bash
@@ -14,16 +14,19 @@ dash(){ for ((i=1;i<=$1;i++)); do printf "-"; done; }
 
 modsec(){
     local OPTIND
-    while getopts d:hi:l:n:qv option; do
+    while getopts d:D:hi:l:n:qv option; do
       case "${option}" in
         q) QUIET=1 ;;
         v) VERBOSE=1 ;;
-	d) DATE=$(date --date="-${OPTARG} days" +"%a.%b.%d") ;;
+	d) DATE=$(date --date="-${OPTARG} days" +"%a.%b.%d");
+	   echo; date --date="-${OPTARG} days" +"%A, %B %d, %Y -- %Y.%m.%d" ;;
+	D) DOMAIN=${OPTARG} ;;
         i) IP=${OPTARG} ;;
 	l) LOGFILE=${OPTARG} ;;
         n) COUNT=${OPTARG} ;;
         h) echo -e "\n Usage: $0 [OPTIONS]\n
     -d ... <days ago> (1-9...) otherwise assumes today
+    -D ... <domain> to specify when searching errors
     -h ... display this help output and quit
     -i ... <ipaddress> (can be full IP or regex)
     -l ... <logfile> (set alternate log file)
@@ -43,14 +46,14 @@ modsec(){
       printf "$FORMAT" " Count#" " Error#" " Remote-IP" " Error Message";
 
       printf "$FORMAT" "--------" "---------" "$(dash 16)" "$(dash 42)";
-      grep -Ei "$DATE.*client.$IP.*id..[0-9]{6,}\".*\[msg" $LOGFILE\
+      grep -Ei "$DATE.*client.$IP.*id..[0-9]{6,}\".*\[msg.*$DOMAIN" $LOGFILE\
          | perl -pe 's/.*\[client\ (.*?)\].*\[id "([0-9]{6,})"\].*\[msg "(.*?)"\].*/\2\t\1\t\3/' | sed 's/ /_/g'\
          | sort | uniq -c | sort -rn | awk '{printf "%7s   %-8s  %-16s %s\n",$1,$2,$3,$4}' | sed 's/_/ /g' | head -n $COUNT
 
       if [[ -n $nomsg ]]; then
 	echo -e "\nPattern Matches"
         printf "$FORMAT" "--------" "---------" "$(dash 16)" "$(dash 42)";
-        grep -v "\[msg" $LOGFILE | grep -E "$DATE.*client.$IP.*phase\ 2"\
+        grep -v "\[msg" $LOGFILE | grep -E "$DATE.*client.$IP.*phase\ 2.*$DOMAIN"\
            | perl -pe 's/.*\[client\ (.*?)\].*phase.2\).\ (.*?)\[file.*\[id\ "(.*?)"\].*/\3\t\1\t\2/g' | sed 's/ /_/g'\
 	   | sort | uniq -c | sort -rn | awk '{printf "%7s   %-8s  %-16s %s\n",$1,$2,$3,$4}' | sed 's/_/ /g' | head -n $COUNT
       fi
@@ -58,7 +61,7 @@ modsec(){
       if [[ -n $pcre ]]; then
 	echo -e "\nPCRE Limits"
         printf "$FORMAT" "--------" "---------" "$(dash 16)" "$(dash 42)";
-        grep PCRE $LOGFILE | grep -Ei "$DATE.*client.$IP.*id..[0-9]{6,}\""\
+        grep PCRE $LOGFILE | grep -Ei "$DATE.*client.$IP.*id..[0-9]{6,}\".*$DOMAIN"\
            | perl -pe 's/.*\[client\ (.*?)\].*\[id\ "(.*?)"\].*(Execution.*?):.*/\2\t\1\t\3/' | sed 's/ /_/g'\
            | sort | uniq -c | sort -rn | awk '{printf "%7s   %-8s  %-16s %s\n",$1,$2,$3,$4}' | sed 's/_/ /g' | head -n $COUNT
       fi
@@ -70,14 +73,14 @@ modsec(){
         grep -Ei "$DATE.*client.$IP.*\] |id.*[0-9]{6,}\]" $LOGFILE | awk 'BEGIN {RS="]\nc"} {print $4,$2}'\
          | tr -d \] | sort | uniq -c | awk '{printf "%7s   %-8s  %s\n",$1,$2,$3}' | sort -rnk1 | head -n $COUNT;
       else
-        grep -Ei "$DATE.*client.$IP.*id..[0-9]{6,}\"" $LOGFILE | perl -pe 's/.*\[client\ (.*?)\].*\[id\ \"([0-9]{6,})\"\].*/\2 \1/'\
+        grep -Ei "$DATE.*client.$IP.*id..[0-9]{6,}\".*$DOMAIN" $LOGFILE | perl -pe 's/.*\[client\ (.*?)\].*\[id\ \"([0-9]{6,})\"\].*/\2 \1/'\
 	 | sort | uniq -c | awk '{printf "%7s   %-8s  %s\n",$1,$2,$3}' | sort -rn | head -n $COUNT;
       fi
     fi
     echo
 
     if [[ $VERBOSE == 1 ]]; then
-      echo -e "LOGFILE: $LOGFILE\nDATE   : $DATE\nCOUNT  : $COUNT\nIP     : $IP\n"; fi
+      echo -e "LOGFILE: $LOGFILE\nDOMAIN : $DOMAIN\nDATE   : $DATE\nCOUNT  : $COUNT\nIP     : $IP\n"; fi
 }
 modsec "$@"
 
