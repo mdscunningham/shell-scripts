5e197bb976b6bbf1ce9b8a68ba1eb34eea450ee5
mdscunningham <mcunningham@mdsc.info>
2015-09-08 01:35:16 -0400

    Updated fullslash.sh
    * Now uses du -x -k to only do the root partition
    * Also uses awk for formatting output as xx.xxx floating point

--- fullslash.sh
+++ fullslash.sh
@@ -3,24 +3,11 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2015-07-05
-# Updated: 2015-07-05
+# Updated: 2015-09-07
 #
 #
 #!/bin/bash
 # Find large disk objects when '/' is full or close to full
 
-# Enable BASH extended globs
-shopt -s extglob
-
-# Generate list of partitions to exlude
-PARTLIST=$(df -h | awk -F/ 'NR>2 {printf "%s|",$NF}' | sed 's/|$/|proc/g';)
-
-# Summarize disk usage for all non-partition directories in '/'
-du --max-depth 3 -h /!($PARTLIST) | grep --color -E '^[0-9]{3}M|^[0-9\.]*G'
-
-
-## Oneliner-version
-#
-# shopt -s extglob; du --max-depth 3 -h /!($(df -h | awk -F/ 'NR>2 {printf "%s|",$NF}' | sed 's/|$/|proc/g';)) | grep --color -E '^[0-9]{3}M|^[0-9\.]*G'
-#
-##
+if [[ -n $1 ]]; then depth=$1; else depth=1; fi
+du -kx --max-depth $depth / | awk '{printf "%8.3fG %s\n",($1/1000/1024),$2}' | grep -E --color '[1-9]\....G |0\.[1-9]..G '
