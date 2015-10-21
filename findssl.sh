8dede355ab5aca3d7e11c632611e3bd916066342
mdscunningham <mcunningham@mdsc.info>
2015-10-20 21:05:00 -0400

    Updated findssl.sh
    * Now includes workarounds for FTP on port 21, and SMTP on 465/587

--- findssl.sh
+++ findssl.sh
@@ -12,6 +12,7 @@ dash(){ for ((i=1; i<=$1; i++)); do printf "$2"; done; }
 
 # Check for alternate port
 if [[ $2 == '-p' ]]; then P=$3; else P=443; fi
+if [[ $P == 21 ]]; then tls="-starttls ftp"; elif [[ $P == 587 ]]; then tls="-starttls smtp"; else tls=''; fi
 
 # Cleanup Domain input
 D=$(echo $1 | sed 's|^http:||g;s|https:||g;s|\/||g;');
@@ -30,20 +31,20 @@ echo "https://certlogik.com/ssl-checker/$D:$P"
 echo "https://www.ssllabs.com/ssltest/analyze.html?d=$D&latest"
 
 # Connect to the domain at port, get SSL, Decode SSL, clean up output and print
-echo | openssl s_client -connect $D:$P $SNI 2>/dev/null\
+echo | openssl s_client ${tls} -connect $D:$P $SNI 2>/dev/null\
  | awk '/-----BEGIN/,/-----END/ {print}'\
  | openssl x509 -text -noout 2>/dev/null\
  | egrep -i 'subject:|dns:|issuer:'\
  | sed 's/DNS:/\nDNS:/;s/Subject: /\nSubject:\n/;s/Issuer: /\nIssuer:\n/;s/, /\n/g;s/[=:]/: /g;s/\/email/\nemail/g;s/\/busi/\nBusi/g;s/\/seri/\nSeri/g;s/\/1\.3\.6\./\n1\.3\.6\./g';
 
-echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Signature.Algorithm | head -1 | sed 's/.*Sig.*:/\nSignature:/g'
-echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Not.After | sed 's/.*Not.After./Expires/g'
+echo | openssl s_client ${tls} -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Signature.Algorithm | head -1 | sed 's/.*Sig.*:/\nSignature:/g'
+echo | openssl s_client ${tls} -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Not.After | sed 's/.*Not.After./Expires/g'
 
 echo;
 
 # Check for common SSL issues, and their error messages.
 echo "SSL Return Code"; dash 40 -; echo
-rcode=$(echo | openssl s_client -nbio -connect $D:$P $SNI 2>/dev/null | grep Verify.*)
+rcode=$(echo | openssl s_client -nbio ${tls} -connect $D:$P $SNI 2>/dev/null | grep Verify.*)
 echo $rcode
 if [[ $(echo $rcode | awk '{print $4}') =~ [0-9]{2} ]]; then
   curl -s https://www.openssl.org/docs/apps/verify.html | grep -A4 "$(echo $rcode | awk '{print $4}') X509" | grep -v X509 | sed 's/<[^>]*>//g' | tr '\n' ' '; echo;
