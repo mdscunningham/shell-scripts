bdfcb0d29c32ca423f49ecd421e18db8d3df6156
mdscunningham <mcunningham@mdsc.info>
2015-11-17 20:36:35 -0500

    Updated: diskhogs.sh
    * Added functions to make stand-alone
    * Added $PWD instead of . to give full paths to files/folders

--- diskhogs.sh
+++ diskhogs.sh
@@ -3,10 +3,13 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-07-12
-# Updated: 2014-07-12
+# Updated: 2015-11-17
 #
 #
 #!/bin/bash
+
+getusr(){ pwd | sed 's:^/chroot::' | cut -d/ -f3; }
+dash(){ for ((i=1;i<=$1;i++)); do printf "-"; done; }
 
     if [[ "$@" =~ "-h" ]]; then
         echo -e "\n Usage: diskhogs [maxdepth] [-d]\n";
@@ -18,11 +21,11 @@
         DEPTH=3;
     fi;
     echo -e "\n---------- Large Directories $(dash 51)";
-    du -h --max-depth $DEPTH | grep -E '[0-9]G|[0-9]{3}M';
+    du -h --max-depth $DEPTH $PWD | grep -E '[0-9]G|[0-9]{3}M';
 
     if [[ ! $@ =~ '-d' ]]; then
     echo -e "\n---------- Large Files $(dash 57)";
-    find . -type f -size +100000k -group $(getusr) -exec ls -lah {} \;;
+    find $PWD -type f -size +100000k -group $(getusr) -exec ls -lah {} \;;
     fi;
 
     echo -e "\n---------- Large Databases $(dash 53)";
