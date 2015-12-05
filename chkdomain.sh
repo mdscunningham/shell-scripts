9d4a901dfc52d42c8770559b4ad0b7f85886e538
mdscunningham <mcunningham@mdsc.info>
2015-12-05 15:15:19 -0500

    Updated chkdomain.sh
    * Added getopts for parsing flags for dnsRecord and account selection
    * Added getusr() function for account selection
    * Corrected wierd padding issue on MailEx output printing

--- chkdomain.sh
+++ chkdomain.sh
@@ -4,7 +4,7 @@
 # Author: Mark David Scott Cunningham                      | M  | D  | S  | C  |
 #                                                          +----+----+----+----+
 # Created: 2015-11-30
-# Updated: 2015-11-30
+# Updated: 2015-12-05
 #
 # Purpose: Gather IP/DNS/Mail informaion for some or all domains on a server
 #
@@ -19,22 +19,39 @@
       BLINK=$(tput blink);      REVERSE=$(tput smso)
   UNDERLINE=$(tput smul)
 
+# Setting defaults
+recordType="MX"
+account=".*"
+
+getusr(){ pwd | sed 's:^/chroot::' | cut -d/ -f3; }
+
+# Parsing input from command line flags
+#local OPTIND
+while getopts a:d:h option; do
+  case "${option}" in
+    a) if [[ ${OPTARG} == '.' ]]; then account=$(getusr); else account=${OPTARG}; fi ;;
+    d) recordType=${OPTARG};;
+    h) echo ;;
+  esac
+done
+
+# Formatting
 FMT="%-15s %-15s %-3s %-6s %-40s %-s\n"
-HIGHLIGHT="${BRIGHT}${RED}%-15s %-15s${NORMAL} %-3s %-6s %-40s %-s\n"
+HIGHLIGHT="${BRIGHT}${RED}%-15s %-15s${NORMAL} %-3s %-6s ${BRIGHT}${RED}%-40s${NORMAL} %-s\n"
 
 # Lookup all the domains on the server
-domainList=$(awk -F: '!/: nobody/ {print $1}' /etc/userdomains)
+domainList=$(awk -F: "/: ${account}/ &&"'!/: nobody/ {print $1}' /etc/userdomains)
 #domainList=$(awk '/ServerName/ {print $2}' /usr/local/apache/conf/httpd.conf | sort | uniq)
 
 # Check if mail is configured for remote or local
 _remoteLocal(){
-  if [[ -n $(grep $1 /etc/localdomains) ]]; then echo "${GREEN}Local${NORMAL}"
+  if [[ -n $(grep $1 /etc/localdomains) ]]; then echo "${GREEN}Local${NORMAL} "
     elif [[ -n $(grep $1 /etc/remotedomains) ]]; then echo "${RED}Remote${NORMAL}"
-    else echo "NoConf"
+    else echo "${BRIGHT}NoConf${NORMAL}"
   fi
 }
 
-printf "\n$FMT" " Vhost-IP" " DNS-IP" "SSL" "MailEx" " DNS-MX" " Domain"
+printf "\n$FMT" " Vhost-IP" " DNS-IP" "SSL" "MailEx" " Domain" " DNS-${recordType}"
 printf "$FMT" "---------------" "---------------" "---" "------" "----------------------------------------" "---------------------------------------------"
 
 # Loop through the list of domains and gather information.
@@ -49,11 +66,11 @@ for domain in $domainList; do
   dnsIP=$(dig +short +time=1 +tries=1 $domain | grep [0-9] | head -1;)
 
   # Find the DNS MX record
-  mxRecord=$(dig mx +short +time=1 +tries=1 $domain | grep [0-9] | head -1;)
+  dnsRecord=$(dig $recordType +short +time=1 +tries=1 $domain | tail -1;)
 
-  if [[ $vhostIP == $dnsIP ]]; then 
-    printf "$FMT" "$vhostIP" "$dnsIP" "${ssl:- - }" "$(_remoteLocal $domain)" " $mxRecord" " $domain"
+  if [[ $vhostIP == $dnsIP ]]; then
+    printf "$FMT" "$vhostIP" "$dnsIP" "${ssl:- - }" "$(_remoteLocal $domain)" " $domain" " $dnsRecord"
   else
-    printf "$HIGHLIGHT" "$vhostIP" "$dnsIP" "${ssl:- - }" "$(_remoteLocal $domain)" " $mxRecord" " $domain"
+    printf "$HIGHLIGHT" "$vhostIP" "$dnsIP" "${ssl:- - }" "$(_remoteLocal $domain)" " $domain" " $dnsRecord"
   fi
 done; echo
