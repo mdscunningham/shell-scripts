61dc4e02f251c149bbd6af245776834d23034fc1
mdscunningham <mcunningham@mdsc.info>
2016-01-28 00:08:35 -0500

    Updated emailtest.sh
    * Added check for DH key strength
    * Commented out output of manual connection steps

--- emailtest.sh
+++ emailtest.sh
@@ -3,7 +3,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-02-08
-# Updated: 2014-11-07
+# Updated: 2016-01-27
 #
 #
 #!/bin/bash
@@ -19,11 +19,13 @@ if [[ -n $1 && $1 != "-h" ]]; then
   if [[ -z $4 ]]; then read -p "Password: " emailpass; else emailpass=$4; fi
 fi
 
+echo
 case $1 in
   -i|--imap   )
-	echo -e "\n  1) [tag] LOGIN <address> <password>\n  2) [tag] LIST \"\" \"*\"\n  3) [tag] SELECT INBOX\n  4) [tag] FETCH 1 BODY[]\n  5) [tag] LOGOUT\n"
+	#echo -e "\n  1) [tag] LOGIN <address> <password>\n  2) [tag] LIST \"\" \"*\"\n  3) [tag] SELECT INBOX\n  4) [tag] FETCH 1 BODY[]\n  5) [tag] LOGOUT\n"
+	echo | openssl s_client -connect $host:993 -cipher "EDH" 2>/dev/null | grep "Server Temp Key"; echo
 	expect -c "
-          spawn openssl s_client -tls1 -connect $host:993 -quiet
+          spawn openssl s_client -connect $host:993 -quiet
 	expect OK
 	  send \"tag LOGIN ${emailaddr} ${emailpass}\r\"
 	expect OK
@@ -35,9 +37,10 @@ case $1 in
 	interact
         ";;
   -p|--pop    )
-	echo -e "\n  1) USER <address>\n  2) PASS <password>\n  3) STAT\n  4) LIST\n  5) RETR 1\n  6) QUIT\n";
+	#echo -e "\n  1) USER <address>\n  2) PASS <password>\n  3) STAT\n  4) LIST\n  5) RETR 1\n  6) QUIT\n";
+	echo | openssl s_client -connect $host:995 -cipher "EDH" 2>/dev/null | grep "Server Temp Key"; echo
 	expect -c "
-	  spawn openssl s_client -tls1 -connect $host:995 -quiet
+	  spawn openssl s_client -connect $host:995 -quiet
 	expect OK
 	  send \"USER ${emailaddr}\r\"
 	expect OK
@@ -51,13 +54,14 @@ case $1 in
 	interact
         ";;
   -s|--smtp   )
-	echo -e "\n  1) HELO $host\n  2) MAIL FROM:<address>\n  3) RCPT TO:<address>\n  4) DATA\n  5) .\n  6) QUIT\n";
+	#echo -e "\n  1) HELO $host\n  2) MAIL FROM:<address>\n  3) RCPT TO:<address>\n  4) DATA\n  5) .\n  6) QUIT\n";
+	echo | openssl s_client -starttls smtp -connect $host:587 -cipher "EDH" 2>/dev/null | grep "Server Temp Key"; echo
 	newuser=$(echo -n $emailaddr | base64)
 	newpass=$(echo -n $emailpass | base64)
 	expect -c "
 	  spawn openssl s_client -starttls smtp -connect $host:587 -quiet
 	expect 250
-	  send \"HELO $host\r\"
+	  send \"EHLO $host\r\"
 	expect 250
 	  send \"AUTH LOGIN\r\"
 	expect 334
@@ -73,6 +77,7 @@ case $1 in
  Usage: _emailtest [option] [hostname] [address] [password]\n
     -i|--imap ... Test IMAP-SSL to [hostname]
     -p|--pop .... Test POP3-SSL to [hostname]
-    -s|--smtp ... Test SMTP-TLS to [hostname]\n"
+    -s|--smtp ... Test SMTP-TLS to [hostname]"
     ;;
 esac
+echo
