ed5ae8e4729ee72340f6986f58652af2f1ddffa8
mdscunningham <mcunningham@mdsc.info>
2016-02-12 03:18:38 -0500

    Updated dbpass-update.sh
    * Cleaned up indentation to be standart (2) spaces instead of (8)

--- dbpass-update.sh
+++ dbpass-update.sh
@@ -10,68 +10,71 @@
 #
 
 backup_prompt(){
-	echo;
-	read -p "cPanel User: " username
-	read -p "Database: " database
-	echo;
-	}
+  echo;
+  read -p "cPanel User: " username
+  read -p "Database: " database
+  echo;
+  }
 
 backup(){
-	echo -n > /root/${username}.dbpass.log
-	echo "Logging to /root/${username}.dbpass.log"
-	grep -rl $database /home/${username}/public_html/ | while read files; do
-		echo "$files -> ${files}.bak" | tee -a /root/${username}.dbpass.log
-		cp -a $files{,.bak};
-	done; echo
-	}
+  echo -n > /root/${username}.dbpass.log
+  echo "Logging to /root/${username}.dbpass.log"
+  grep -rl $database /home/${username}/public_html/ | while read files; do
+    echo "$files -> ${files}.bak" | tee -a /root/${username}.dbpass.log
+    cp -a $files{,.bak};
+  done; echo
+  }
 
 update_prompt(){
-	echo;
-	read -p "cPanel User: " username
-	read -p "Database: " database
-	read -p "Old Password: " oldpass
-	read -p "New Password: " newpass
-	echo;
-	}
+  echo;
+  read -p "cPanel User: " username
+  read -p "Database: " database
+  read -p "Old Password: " oldpass
+  read -p "New Password: " newpass
+  echo;
+  }
 
 update(){
-	grep -rl $database /home/${username}/public_html/ | grep -Ev '*.bak$' | while read files; do
-		echo $files;
-		grep $oldpass $files;
-		echo "updating to ...";
-		grep $oldpass $files | sed "s/$oldpass/$newpass/g";
-		sed -i "s/$oldpass/$newpass/g" $files;
-		echo;
-	done; echo
-	}
+  grep -rl $database /home/${username}/public_html/ | grep -Ev '*.bak$' | while read files; do
+    echo $files;
+    grep $oldpass $files;
+    echo "updating to ...";
+    grep $oldpass $files | sed "s/$oldpass/$newpass/g";
+    sed -i "s/$oldpass/$newpass/g" $files;
+    echo;
+  done; echo
+  }
 
 restore(){
-	echo;
-	read -p "cPanel User: " username
-	echo;
-	if [[ -f /root/${username}.dbpass.log ]]; then
-		awk '{print $1}' /root/${username}.dbpass.log | while read files; do
-			echo "Removing $files"; rm -f $files;
-			echo "Renaming $files.bak -> $files"; mv $files{.bak,}
-			echo;
-		done;
-	else
-		echo "Missing: /root/${username}.dbpass.log"
-	fi
-	}
+  echo;
+  read -p "cPanel User: " username
+  echo;
+  if [[ -f /root/${username}.dbpass.log ]]; then
+    awk '{print $1}' /root/${username}.dbpass.log | while read files; do
+      echo "Removing $files";
+      rm -f $files;
+      echo "Renaming $files.bak -> $files";
+      mv $files{.bak,};
+      echo;
+    done;
+  else
+    echo "Missing: /root/${username}.dbpass.log"
+  fi
+  }
 
 cleanup(){
-	echo;
-        read -p "cPanel User: " username
-        echo;
-        if [[ -f /root/${username}.dbpass.log ]]; then
-                awk '{print $NF}' /root/${username}.dbpass.log | while read files; do
-                        echo "Removing ${files}"; rm -f ${files};
-                done; echo;
-        else
-                echo "Missing: /root/${username}.dbpass.log"
-        fi
-	}
+  echo;
+  read -p "cPanel User: " username
+  echo;
+  if [[ -f /root/${username}.dbpass.log ]]; then
+    awk '{print $NF}' /root/${username}.dbpass.log | while read files; do
+      echo "Removing ${files}";
+      rm -f ${files};
+    done; echo;
+  else
+    echo "Missing: /root/${username}.dbpass.log"
+  fi
+  }
 
 echo "
 [1] Backup files
@@ -82,16 +85,16 @@ echo "
 read -p "[1-4]: " opt;
 
 if [[ $opt == 1 ]]; then
-	backup_prompt;
-	backup;
+  backup_prompt;
+  backup;
 elif [[ $opt == 2 ]]; then
-	update_prompt;
-	update;
+  update_prompt;
+  update;
 elif [[ $opt == 3 ]]; then
-	restore;
+  restore;
 elif [[ $opt == 4 ]]; then
-	cleanup;
+  cleanup;
 else
-	echo -e "\n$opt is not a valid option.";
-	echo -e "Please enter a valid slection.\n";
+  echo -e "\n$opt is not a valid option.";
+  echo -e "Please enter a valid slection.\n";
 fi;
