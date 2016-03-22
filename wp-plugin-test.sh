f837c37442053ced02ccf560beb67c0251af24fb
mdscunningham <mcunningham@mdsc.info>
2016-03-22 04:33:19 -0400

    Updated wp-plugin-test.sh
    * Corrected corner case on domain lookup from path shared by domains

--- wp-plugin-test.sh
+++ wp-plugin-test.sh
@@ -31,19 +31,19 @@ if [[ ! $username && ! $domain ]]; then
 elif [[ $username && ! $domain ]]; then
   userdir="/home/$username/public_html"
 elif [[ $domain ]]; then
-  userdir=$(grep -C5 $domain /usr/local/apache/conf/httpd.conf | awk '/DocumentRoot/ {print $2}' | head -1)
+  userdir=$(grep -C5 " ${domain}" /usr/local/apache/conf/httpd.conf | awk '/DocumentRoot/ {print $2}' | head -1)
 fi
 
 # Lookup domain name
 if [[ ! $domain ]]; then
-  domain=$(grep -C5 $userdir /usr/local/apache/conf/httpd.conf | awk '/ServerName/ {print $2}');
+  domain=$(grep -C5 ${userdir} /usr/local/apache/conf/httpd.conf | awk '/ServerName/ {print $2}' | tail -1);
 fi
 
 # Get list of plugin directories
-dirs=$(find $userdir/wp-content/plugins/* -maxdepth 0 -type d -print);
+dirs=$(find ${userdir}/wp-content/plugins/* -maxdepth 0 -type d -print);
 
 
-if [[ ! $(ip a | grep $(dig +short $domain) 2>/dev/null) && ! $(grep " $domain" /etc/hosts 2>/dev/null) ]]; then
+if [[ ! $(ip a | grep $(dig +time=2 +tries=2 +short $domain) 2>/dev/null) && ! $(grep " $domain" /etc/hosts 2>/dev/null) ]]; then
   echo -e "\n  Looks like the domain is not live on this server, make\n  sure to update /etc/hosts for testing from the server.\n"
 else
   # Disable all the plugins
