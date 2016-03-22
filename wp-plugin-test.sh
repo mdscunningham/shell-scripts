df87b52ddc90d72928acb21c84613364812194cb
mdscunningham <mcunningham@mdsc.info>
2016-03-22 01:06:15 -0400

    Updated wp-plugin-test.sh
    * Added command line flags for -u user, and -d domain
    * Added check if site is live on server or not (hosts edit necessary)

--- wp-plugin-test.sh
+++ wp-plugin-test.sh
@@ -10,29 +10,62 @@
 #          one(s) may be breaking site functionality.
 #
 
+# http://inchoo.net/dev-talk/tools/git-deployment/17566/
+
+## OMG OMG OMG!
+## http://stackoverflow.com/questions/16654607/using-getopts-inside-a-bash-function
+
+#!/bin/bash
+
+while getopts d:u: option do
+  case "${option}" in
+    d) domain=${OPTARG};;
+    u) username=${OPTARG};;
+    h) echo "
+  Usage: $0 [OPTIONS] [ARGUMENTS]
+    -u ... <username> Specify username
+    -d ... <domain>   Sepcify domain name
+
+    -h ... Print this help and quit
+       "; exit ;;
+  esac
+done
+
+# Lookup user's homedir
+if [[ ! $username ]]; then
+  userdir=$PWD; else userdir="/home/$username/public_html"
+fi
+
 # Lookup domain name
-domain=$(grep -C5 $PWD /usr/local/apache/conf/httpd.conf | awk '/ServerName/ {print $2}');
+if [[ ! $domain ]]; then
+  domain=$(grep -C5 $userdir /usr/local/apache/conf/httpd.conf | awk '/ServerName/ {print $2}');
+fi
 
 # Get list of plugin directories
-dirs=$(find $PWD/wp-content/plugins/* -maxdepth 0 -type d -print);
+dirs=$(find $userdir/wp-content/plugins/* -maxdepth 0 -type d -print);
 
-# Disable all the plugins
-echo -e "\nDisabling All Plugins";
-for plugin in $dirs;
-  do mv $plugin{,.disable};
-done
 
-# Re-enable each plugin individually and test the site
-for plugin in $dirs; do
-  echo -e "\nRe-Enabling :: $plugin";
-  mv $plugin{.disable,};
-  echo "Testing $domain:";
-  curl -sILk $domain | egrep --color "HTTP/1.[01] [2345][0-9]{2}";
-  mv $plugin{,.disable};
-done;
+if [[ ! $(ip a | grep $(dig +short $domain)) ]]; then
+  echo -e "\n  Looks like the domain is not live on this server, make\n  sure to update /etc/hosts for testing from the server.\n"
+else
+  # Disable all the plugins
+  echo -e "\nDisabling All Plugins";
+  for plugin in $dirs;
+    do mv $plugin{,.disable};
+  done
 
-# Re-Enable all plugins
-echo -e "\nRe-Enabling All Plugins";
-for plugin in $dirs;
-  do mv $plugin{.disable,};
-done
+  # Re-enable each plugin individually and test the site
+  for plugin in $dirs; do
+    echo -e "\nRe-Enabling :: $plugin";
+    mv $plugin{.disable,};
+    echo "Testing $domain:";
+    curl -sILk $domain | egrep --color "HTTP/1.[01] [2345][0-9]{2}";
+    mv $plugin{,.disable};
+  done;
+
+  # Re-Enable all plugins
+  echo -e "\nRe-Enabling All Plugins";
+  for plugin in $dirs;
+    do mv $plugin{.disable,};
+  done
+fi
