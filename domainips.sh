6770bee4a31f277424198e66106e32f300e17716
mdscunningham <mcunningham@mdsc.info>
2016-04-13 00:05:44 -0400

    Updated domainips.sh
    * Updated to be compatible with cPanel's single httpd.conf

--- domainips.sh
+++ domainips.sh
@@ -3,13 +3,23 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-04-29
-# Updated: 2014-07-28
+# Updated: 2016-04-12
 #
 #
 #!/bin/bash
 
-echo; for I in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.'); do
+# Taste the rainbow
+      BLACK=$(tput setaf 0);        RED=$(tput setaf 1)
+      GREEN=$(tput setaf 2);     YELLOW=$(tput setaf 3)
+       BLUE=$(tput setaf 4);     PURPLE=$(tput setaf 5)
+       CYAN=$(tput setaf 6);      WHITE=$(tput setaf 7)
+
+     BRIGHT=$(tput bold);        NORMAL=$(tput sgr0)
+      BLINK=$(tput blink);      REVERSE=$(tput smso)
+  UNDERLINE=$(tput smul)
+
+echo; for I in $(ip addr show | awk '/inet / {print $2}' | cut -d/ -f1 | grep -Ev '^127\.|^10\.|^172\.|^192\.168\.'); do
     printf "  ${BRIGHT}${YELLOW}%-15s${NORMAL}  " "$I";
-    D=$(grep -l $I /etc/httpd/conf.d/vhost_[^000_]*.conf | cut -d_ -f2 | sed 's/.conf$//');
+    D=$(grep -A5 "<VirtualHost.*$I" /usr/local/apache/conf/httpd.conf | awk '!'"/$(hostname)/"' && /ServerName/{print $NF}');
     for x in $D; do printf "$x "; done; echo;
 done; echo
