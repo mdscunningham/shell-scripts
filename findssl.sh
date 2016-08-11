2396e5dd4b3c69e1b5b0a61f1af3a1b3fdba3095
mdscunningham <mcunningham@mdsc.info>
2016-08-11 01:56:59 -0400

    Updated findssl.sh
    * Added proper flag parsing with getopt
    * Now can take multiple domains as input like ddns.sh
    * Also now has help output for usage

--- findssl.sh
+++ findssl.sh
@@ -10,42 +10,56 @@
 
 dash(){ for ((i=1; i<=$1; i++)); do printf "$2"; done; }
 
-# Check for alternate port
-if [[ $2 == '-p' ]]; then P=$3; else P=443; fi
+ip=''
+P=443
 
-# Cleanup Domain input
-D=$(echo $1 | sed 's|^http:||g;s|https:||g;s|\/||g;');
-I=$(dig +short $D | grep [0-9])
+OPTIONS=$(getopt -o "hi:p:" -- "$@") # Execute getopt
+eval set -- "$OPTIONS" # Magic
+while true; do # Evaluate the options for their options
+case $1 in
+  -i ) I=$2; ip=1; shift ;;
+  -p ) P=$2; shift ;;
+  -- ) shift; break ;; # More Magic
+  -h|--help|* ) echo -e "\n  Usage: $0 [-i <IPaddress>] [-p <port>] <dom1> <dom2> ...\n"; exit;; # print help info
+esac;
+shift;
+done
 
-# Check if local version of OpenSSL has SNI support
-if [[ $(openssl version | awk '{print $2}') =~ ^1\. ]]; then SNI="-servername $D"; else SNI=''; fi
-
-if [[ $P == 587 ]]; then SNI="$SNI -starttls smtp";
-elif [[ $P == 21 ]]; then SNI="$SNI -starttls ftp"; fi
-
-## if [[ -n $SNI ]]; then echo '(Using SNI)'; else echo '(Not using SNI)'; fi
-
-# Print header
-echo; echo "$D:$P :: $I:$P"; echo "$(dash 60 -)";
-
-# Print SSL checker service URLs
-echo "https://www.sslshopper.com/ssl-checker.html#hostname=$D:$P"
-echo "https://certlogik.com/ssl-checker/$D:$P"
-echo "https://www.ssllabs.com/ssltest/analyze.html?d=${D}&s=${I}&latest"
-
-#readssl="$(echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | awk '/-----BEGIN/,/-----END/{print}')"
-
-# Connect to the domain at port, get SSL, Decode SSL, clean up output and print
-echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout 2>/dev/null | egrep -i 'subject:|dns:|issuer:'\
- | sed 's/DNS:/\nDNS:/;s/Subject: /\nSubject:\n/;s/Issuer: /\nIssuer:\n/;s/, /\n/g;s/[=:]/: /g;s/\/email/\nemail/g;s/\/busi/\nBusi/g;s/\/seri/\nSeri/g;s/\/1\.3\.6\./\n1\.3\.6\./g';
-echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Signature.Algorithm | head -1 | sed 's/.*Sig/\nSig/g'
-echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Not.After | sed 's/.*Not.After./Expires/g'
 echo;
+for domain in $@; do
+  # Cleanup Domain input
+  D=$(echo $domain | sed 's|^http:||g;s|https:||g;s|\/||g;');
 
-# Check for common SSL issues, and their error messages.
-echo "SSL Return Code"; echo "$(dash 60 -)";
-rcode=$(echo | openssl s_client -connect $D:$P $SNI 2>/dev/null | grep Verify.*)
-echo $rcode
-if [[ $(echo $rcode | awk '{print $4}') =~ [0-9]{2} ]]; then
-  curl -s https://www.openssl.org/docs/apps/verify.html | grep -A4 "$(echo $rcode | awk '{print $4}') X509" | grep -v X509 | sed 's/<[^>]*>//g' | tr '\n' ' '; echo;
-fi; echo
+  # If IP not specified lookup IP
+  if [[ ! $ip ]]; then I=$(dig +short $D | grep [0-9]); fi
+
+  # Check if local version of OpenSSL has SNI support
+  if [[ $(openssl version | awk '{print $2}') =~ ^1\. ]]; then SNI="-servername $D"; else SNI=''; fi
+
+  # Add some TLS options if special ports are specified
+  if [[ $P == 587 ]]; then SNI="$SNI -starttls smtp";
+  elif [[ $P == 21 ]]; then SNI="$SNI -starttls ftp"; fi
+
+  # Print header
+  echo "$(dash 80 =)"; echo "$D:$P :: $I:$P"; echo "$(dash 80 -)";
+
+  # Print SSL checker service URLs
+  echo "https://www.sslshopper.com/ssl-checker.html#hostname=$D:$P"
+  echo "https://certlogik.com/ssl-checker/$D:$P"
+  echo "https://www.ssllabs.com/ssltest/analyze.html?d=${D}&s=${I}&latest"
+
+  # Connect to the IP at port; get SSL; Decode SSL; clean up output and print
+  echo | openssl s_client -connect $I:$P $SNI 2>/dev/null | openssl x509 -text -noout 2>/dev/null | egrep -i 'subject:|dns:|issuer:'\
+   | sed 's/DNS:/\nDNS:/;s/Subject: /\nSubject:\n/;s/Issuer: /\nIssuer:\n/;s/, /\n/g;s/[=:]/: /g;s/\/email/\nemail/g;s/\/busi/\nBusi/g;s/\/seri/\nSeri/g;s/\/1\.3\.6\./\n1\.3\.6\./g';
+  echo | openssl s_client -connect $I:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Signature.Algorithm | head -1 | sed 's/.*Sig/\nSig/g'
+  echo | openssl s_client -connect $I:$P $SNI 2>/dev/null | openssl x509 -text -noout | grep Not.After | sed 's/.*Not.After./Expires/g'
+  echo;
+
+  # Check for common SSL issues, and their error messages.
+  echo "SSL Return Code"; echo "$(dash 80 -)";
+  rcode=$(echo | openssl s_client -connect $I:$P $SNI 2>/dev/null | grep Verify.*)
+  echo $rcode
+  if [[ $(echo $rcode | awk '{print $4}') =~ [0-9]{2} ]]; then
+    curl -s https://www.openssl.org/docs/apps/verify.html | grep -A4 "$(echo $rcode | awk '{print $4}') X509" | grep -v X509 | sed 's/<[^>]*>//g' | tr '\n' ' '; echo;
+  fi; echo
+done
