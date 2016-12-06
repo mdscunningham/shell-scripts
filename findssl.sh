ed830e2e37c99bfd1bc6dac842212245dfec4b3c
mdscunningham <mcunningham@mdsc.info>
2016-12-06 17:12:25 -0500

    Updated findssl.sh
    * If IP given as domain, skip the IP lookup and just use the IP as is

--- findssl.sh
+++ findssl.sh
@@ -4,7 +4,7 @@
 # Author: Mark David Scott Cunningham			   | M  | D  | S  | C  |
 # 							   +----+----+----+----+
 # Created: 2014-03-29
-# Updated: 2016-08-21
+# Updated: 2016-12-06
 #
 # Purpose: Test SSL connection and cert loading on a particular server and port
 
@@ -49,7 +49,7 @@ for domain in $@; do
   D=$(echo $domain | sed 's|^http:||g;s|https:||g;s|\/||g;');
 
   # If IP not specified lookup IP
-  if [[ ! $ip ]]; then I=$(dig +short $D | grep [0-9] | head -1); fi
+  if [[ ! $ip && $D =~ [a-z] ]]; then I=$(dig +short $D | grep [0-9] | head -1); else I=$D; fi
 
   # Check if local version of OpenSSL has SNI support
   if [[ $(openssl version | awk '{print $2}') =~ ^1\. ]]; then SNI="-servername $D"; else SNI=''; fi
