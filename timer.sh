7a5407274865dd756ffd57bbac5be5447cc74184
mdscunningham <mcunningham@mdsc.info>
2016-12-18 23:24:45 -0500

    Updated timer.sh
    * Added hour display to the tab title timer script

--- timer.sh
+++ timer.sh
@@ -4,11 +4,12 @@
 # Author: Mark David Scott Cunningham                      | M  | D  | S  | C  |
 #                                                          +----+----+----+----+
 # Created: 2016-04-30
-# Updated: 2016-04-30
+# Updated: 2016-12-18
 #
 # Purpose: Put a count-up timer into the title bar
 #
 
+h=0;
 m=0;
 s=0;
 while true; do
@@ -18,7 +19,21 @@ while true; do
     ((m++));
     s=0;
   fi;
+
+  if [[ $[$m % 60] == 0 && $[$s % 60] == 0 ]]; then
+    ((h++));
+    m=0;
+  fi;
+
   clear;
-  printf "\033]0;$m:$s\007";
+  printf "\033]0;"
+  for t in $h $m $s; do
+    if [[ $t -lt 10 ]]; then
+      printf "0$t:";
+    else
+      printf "$t:";
+    fi;
+  done
+  printf "\007"
 done
 
