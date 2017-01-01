a1b0ee5c71053997b3dcd3ae9a0202a28aabd34a
mdscunningham <mcunningham@mdsc.info>
2017-01-01 13:13:37 -0500

    Updated ocspcheck.sh
    * Changed temp file location to actually be in /tmp/

--- ocspcheck.sh
+++ ocspcheck.sh
@@ -4,26 +4,26 @@
 # Author: Mark David Scott Cunningham                      | M  | D  | S  | C  |
 #                                                          +----+----+----+----+
 # Created: 2016-12-26
-# Updated: 2016-12-27
+# Updated: 2016-12-28
 #
 # Purpose: Check if SSL is revoked by making an OCSP requet to the SSL issuer
 #
 
 domain=$1;
 
-echo | openssl s_client -connect $domain:443 -showcerts 2>/dev/null| awk '/BEGIN/,/END/ {print}' > fullchain.pem
-  cat fullchain.pem | openssl x509 > $domain.crt
+echo | openssl s_client -connect $domain:443 -showcerts 2>/dev/null| awk '/BEGIN/,/END/ {print}' > /tmp/fullchain.pem
+  cat /tmp/fullchain.pem | openssl x509 > /tmp/$domain.crt
 
 linenum=$(grep -n BEGIN fullchain.pem | awk -F: 'NR==2 {print $1}')
-  tail -n +$linenum fullchain.pem > chain.pem
+  tail -n +$linenum /tmp/fullchain.pem > /tmp/chain.pem
 
-ocspurl=$(openssl x509 -in $domain.crt -noout -ocsp_uri)
+ocspurl=$(openssl x509 -in /tmp/$domain.crt -noout -ocsp_uri)
   echo "OCSP URL : $ocspurl"
 ocsphost=$(echo $ocspurl | cut -d/ -f3)
   echo "OCSP HOST: $ocsphost"
 
-openssl ocsp -no_nonce -header host $ocsphost -issuer chain.pem -cert $domain.crt -url $ocspurl -CAfile chain.pem 2>/dev/null
+openssl ocsp -no_nonce -header host $ocsphost -issuer /tmp/chain.pem -cert /tmp/$domain.crt -url $ocspurl -CAfile /tmp/chain.pem 2>/dev/null
 
-rm -f fullchain.pem
-rm -f chain.pem
-rm -f $domain.crt
+rm -f /tmp/fullchain.pem
+rm -f /tmp/chain.pem
+rm -f /tmp/$domain.crt
