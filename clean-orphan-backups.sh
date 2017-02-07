e6a130c8ab84f7c4899ec287b0e03af9e1b88276
mdscunningham <mcunningham@mdsc.info>
2017-02-06 20:19:11 -0500

    Updated clean-orphan-backups.sh
    * Added check for legacy and modern backups

--- clean-orphan-backups.sh
+++ clean-orphan-backups.sh
@@ -4,12 +4,20 @@
 # Author: Mark David Scott Cunningham                      | M  | D  | S  | C  |
 #                                                          +----+----+----+----+
 # Created: 2016-09-11
-# Updated: 2016-09-11
+# Updated: 2017-01-24
 #
 # Purpose: Cleaning orphaned newer style backups, for accounts not on the server.
 #
 
-for x in /backup/*/accounts/*.tar.gz /backup/weekly/*/accounts/*.tar.gz; do
+if [[ -d /backup/cpbackup/ ]]; then
+   echo "Cleaning Legacy Backups"
+   backuplist=$(find /backup/cpbackup/{daily,weekly,monthly}/ -type f -name "*.tar.gz" 2>/dev/null)
+else
+   echo "Cleaning Modern Backups"
+   backuplist=$(find /backup/{,weekly,monthly}/*/accounts/ -type f -name "*.tar.gz" 2>/dev/null)
+fi
+
+for x in $backuplist; do
   user=$(basename $x .tar.gz);
   if [[ ! $(grep $user /etc/domainusers) ]]; then
    echo "Removed orphaned backup :: $user :: $x";
