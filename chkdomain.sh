fbe0c348fcccc174e6eae1bdef225df26c1d8428
mdscunningham <mcunningham@mdsc.info>
2017-02-13 23:38:11 -0500

    Updated chkdomain.sh
    * Fixed display issue with Sucuri IPs, the output line wrapped and broke the formatting

--- chkdomain.sh
+++ chkdomain.sh
@@ -4,7 +4,7 @@
 # Author: Mark David Scott Cunningham                      | M  | D  | S  | C  |
 #                                                          +----+----+----+----+
 # Created: 2015-11-30
-# Updated: 2016-08-16
+# Updated: 2017-02-13
 #
 # Purpose: Gather IP/DNS/Mail informaion for some or all domains on a server
 #
@@ -140,7 +140,7 @@ for domain in $domainList; do
       printf "$FMT" "$vhostIP" "$dnsIP" "${ssl:- - }" "$(_remoteLocal $domain)" "$domType" "$domain" "$addInfo"
     elif [[ $vhostIP != $dnsIP ]]; then
       # Check if the domain is using a masking service and then report that
-      masking=$(curl -s ipinfo.io/$dnsIP | grep -Eio 'cloudflare|incapsula|sucuri')
+      masking=$(curl -s ipinfo.io/$dnsIP | grep -Eio 'cloudflare|incapsula|sucuri' | tail -1)
       if [[ $masking ]]; then dnsIP=$masking; fi
       printf "$HIGHLIGHT" "$vhostIP" "$dnsIP" "${ssl:- - }" "$(_remoteLocal $domain)" "$domType" "$domain" "$addInfo"
     fi
